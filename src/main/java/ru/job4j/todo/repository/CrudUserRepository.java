ca175db5ca6bd786592353468cf79440d9f6584b
Boris <boris86msk@yandex.ru>
2023-12-13 23:02:18 +0300

    add crud repository

---
+++ src/main/java/ru/job4j/todo/repository/CrudUserRepository.java
@@ -0,0 +1,39 @@
+package ru.job4j.todo.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.job4j.todo.model.User;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class CrudUserRepository {
+    private final CrudRepository crudRepository;
+
+    public CrudUserRepository(CrudRepository crudRepository) {
+        this.crudRepository = crudRepository;
+    }
+
+    /**
+     * Сохранить в базе.
+     * @param user пользователь.
+     * @return пользователь с id.
+     */
+    public Optional<User> save(User user) {
+        crudRepository.run(session -> session.persist(user));
+        return Optional.of(user);
+    }
+
+    /**
+     * Найти пользователя по login.
+     * @param login login.
+     * @param pass  password.
+     * @return Optional or user.
+     */
+    public Optional<User> findByLoginAndPassword(String login, String pass) {
+        return crudRepository.optional(
+                "from User where login = :fLogin and password = :fPass", User.class,
+                Map.of("fLogin", login, "fPass", pass)
+        );
+    }
+}
